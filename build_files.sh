c2a9277596d953fcefc12c702653bb18708569c1
Rohit <rohit.kalavapudi@gmail.com>
2023-09-24 17:03:23 +0530

    eleven

--- build_files.sh
+++ build_files.sh
@@ -119,8 +119,7 @@ pip install zope.interface==6.0
 pip3.9 install psycopg2-binary
 sudo apt-get install libpq-dev
 sudo yum install postgresql-devel
-pip3.9 install psycopg2
-pip3.9 install --install-option="--pg-config=/path/to/pg_config" psycopg2
+pip install --install-option="--pg-config=/path/to/pg_config" psycopg2
 
 pip install psycopg2 
 pip install psycopg2-binary
