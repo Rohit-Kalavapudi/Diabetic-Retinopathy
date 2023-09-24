21ea97ba86752d8229c08cbf853cb7566bed5a4d
Rohit <rohit.kalavapudi@gmail.com>
2023-09-24 16:05:40 +0530

    thrid

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,18 @@
 pip install -r requirements.txt
 pip install django
+pip install django==4.0.7
+pip install home
+pip install datetime
+pip install django.contrib
+pip install pickle
+pip install django.core.files
+pip install tensorflow==2.9.1
+pip install keras==2.9.0
+pip install tensorflow_hub==0.12.0
+pip install numpy==1.22.3
+pip install cv2==4.6.0
+pip install os
+pip install PIL==9.1.0
+pip install matplotlib==3.5.1
+pip install matplotlib.image
 python3.9 manage.py collectstatic
